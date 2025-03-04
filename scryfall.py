e36a1fc8c2ca23af18df9a357b167b90390fcbef
barontate <stuggitg4@gmail.com>
2025-03-04 02:11:48 +0000

    fixed scryfall search

--- scryfall.py
+++ scryfall.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 SCRYFALL_API_URL = "https://api.scryfall.com/cards/"
 
@@ -21,3 +22,22 @@ def fetch_scryfall_data(scryfall_id):
         return card_name
     else:
         return "Not Found", "Unknown"
+
+
+def process_csv(file):
+    """Process the uploaded CSV file, fetch details from Scryfall, and modify the data."""
+    df = pd.read_csv(file)
+    
+    new_data = []
+    for _, row in df.iterrows():
+        scryfall_id = row['Scryfall ID']
+        card_name = row['Name']
+        set_name = row['Set name']
+        foil = row['Foil']
+        quantity = row['Quantity']
+        
+        card_name = fetch_scryfall_data(scryfall_id)
+        
+        new_data.append([card_name, set_name, foil, quantity])
+    
+    return pd.DataFrame(new_data, columns=["Card Name", "Set Name", "Foil", "Quantity"])
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,5 @@
 import streamlit as st
-from scryfall import fetch_scryfall_data
-import pandas as pd
-import time
-import random
-
-st.title("MTGMate Buylist Checker")
-
-
-
-uploaded_file = st.file_uploader("Upload a CSV file of your cards in Manabox format", type=["csv"])
-
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-def process_csv(file):
-    """Process the uploaded CSV file, fetch details from Scryfall, and modify the data."""
-    df = pd.read_csv(file)
-    
-    new_data = []
-    for _, row in df.iterrows():
-        scryfall_id = row['Scryfall ID']
-        card_name = row['Name']
-        set_name = row['Set name']
-        foil = row['Foil']
-        quantity = row['Quantity']
-        
-        card_data = fetch_scryfall_data(scryfall_id)
-        if card_data:
-            border_color = card_data.get("border_color", "")
-            if border_color == "borderless":
-                card_name += " (Borderless)"
-        
-        new_data.append([card_name, set_name, foil, quantity])
-    
-    return pd.DataFrame(new_data, columns=["Card Name", "Set Name", "Foil", "Quantity"])
+from scryfall import process_csv
     
 # Streamlit UI
 st.title("MTG Card CSV Processor with Scryfall API")
