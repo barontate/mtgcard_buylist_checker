93476bc94a4886f737844a8f9d473948fdff25a8
barontate <stuggitg4@gmail.com>
2025-03-04 01:49:54 +0000

    changed title and added CSV upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import scryfall
 
-st.title("🎈 My new app")
+st.title("MTGMate Buylist Checker")
+
+
+
+uploaded_file = st.file_uploader("Upload a CSV file of your cards in Manabox format", type=["csv"])
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
