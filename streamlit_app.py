7d1c2324a3d0eac08fb510411b5e92fb8f857099
barontate <stuggitg4@gmail.com>
2025-03-04 02:24:34 +0000

    updating user instructions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from scryfall import process_csv
     
 # Streamlit UI
-st.title("MTG Card CSV Processor with Scryfall API")
-st.write("Please input a CSV file ")
+st.title("MTG Card CSV Processor for use with Buylist Search")
 
 uploaded_file = st.file_uploader("Upload your CSV file in the Manabox format", type=["csv"])
+
+
 
 if uploaded_file is not None:
     st.write("Processing file...")
@@ -17,3 +18,7 @@ if uploaded_file is not None:
     # Provide download link
     csv = processed_df.to_csv(index=False).encode('utf-8')
     st.download_button("Download Processed CSV", csv, "processed_cards.csv", "text/csv")
+
+st.write("This app should correctly append the border treatment to any card name so that it will search correctly on the MTGMate buy list.")
+
+st.write("TODO: add support for etched foils")
